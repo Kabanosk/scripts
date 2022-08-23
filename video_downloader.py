81c8c113b7c39be6161a192633568cc31e58e818
kabanosk <fiolkawojciech@gmail.com>
2022-08-23 22:02:47 +0200

    Add mp3 argument which change all downloaded videos to mp3 format

--- video_downloader.py
+++ video_downloader.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import concurrent.futures
 
 from pytube import YouTube
-
+from moviepy.editor import VideoFileClip
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Download videos from all urls from file (only youtube urls)")
@@ -13,15 +13,20 @@ def parse_arguments():
         help="File with urls (.txt format)"
     )
     parser.add_argument(
+        '--url', '-u',
+        type=str,
+        help="Url address to video which will be downloaded"
+    )
+    parser.add_argument(
         '--output_dir', '-o',
         type=Path,
         default=Path('.'),
         help="Directory in which videos will be downloaded"
     )
     parser.add_argument(
-        '--url', '-u',
-        type=str,
-        help="Url address to video which will be downloaded"
+        '--mp3',
+        action='store_true',
+        help="Add this argument if you want to download only audio"
     )
     return parser.parse_args()
 
@@ -45,26 +50,40 @@ def replace_whitespaces(text):
 
 
 def download_video(url):
+    global mp3, output_path
+
     yt = YouTube(url)
-    res = yt.streams.get_highest_resolution().resolution
-    yt.streams\
-        .filter(file_extension='mp4')\
-        .get_by_resolution(res)\
-        .download(output_path)
-    return f"Video {yt.title} by {yt.author} downloaded"
+    filename = yt.title
+    max_res = yt.streams.get_highest_resolution().resolution
+    min_res = yt.streams.get_lowest_resolution().resolution
+
+    stream = yt.streams.filter(file_extension='mp4').get_by_resolution(min_res if mp3 else max_res)
+
+    video_path = Path(stream.download(output_path))
+
+    if mp3:
+        audio_path = video_path.with_suffix('.mp3')
+
+        with VideoFileClip(str(video_path)) as vclip:
+            audioclip = vclip.audio
+            audioclip.write_audiofile(str(audio_path))
+            audioclip.close()
+        video_path.unlink()
+
+    return f"\n\t{filename} downloaded"
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    global output_path
+    mp3 = args.mp3
     output_path = args.output_dir
 
     check_arguments(args.urls_file, args.output_dir)
 
     if args.url:
-        download_video(args.url)
+        print(download_video(args.url))
 
-    if output_path and args.urls_file:
+    if args.urls_file:
         with open(args.urls_file, 'r') as f:
             urls = [replace_whitespaces(x) for x in f.readlines()]
 
